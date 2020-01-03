04d830aecfe51e29df0d9141329e755474f1bf4d
keesdekeijzer <git_kdk@xpmail.nl>
2020-01-03 07:32:25 +0100

    tabel makens

--- MySQLi-object.php
+++ MySQLi-object.php
@@ -3,6 +3,19 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
+$dbname = "mijnDB";
+
+// sql voor het maken van de database
+$sql_db = "CREATE DATABASE $dbname";
+
+// sql voor het maken van de tabel
+$sql_tabel = "CREATE TABLE Contacten (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+voornaam VARCHAR(30) NOT NULL,
+achternaam VARCHAR(40) NOT NULL,
+email VARCHAR(60),
+toevoegdatum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
 // maak verbinding
 $conn = new mysqli($servername, $username, $password);
@@ -15,12 +28,18 @@ echo "Succesvol verbonden";
 
 
 // Maak database
-$sql = "CREATE DATABASE mijnDB";
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql_db) === TRUE) {
     echo "Aanmaken database gelukt";
 } else {
     echo "Fout bij het maken van de database: " . $conn->error;
 }
 
+// Maak tabel
+if ($conn->query($sql_tabel) === TRUE) {
+    echo "Aanmaken tabel gelukt";
+} else {
+    echo "Fout bij het aanmaken van de tabel: " . $conn->error;
+}
 
+$conn->close();
 ?> 
--- PDO.php
+++ PDO.php
@@ -3,9 +3,21 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
+$dbname = "mijnDBPDO";
+
+$sql_db = "CREATE DATABASE $dbname";
+
+// sql voor het maken van de tabel
+$sql_tabel = "CREATE TABLE Contacten (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+voornaam VARCHAR(30) NOT NULL,
+achternaam VARCHAR(40) NOT NULL,
+email VARCHAR(60),
+toevoegdatum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // stel de PDO error mode in op exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Succesvol verbonden";
@@ -17,15 +29,23 @@ catch(PDOException $e)
 
 // database maken
 try {
-    $sql = "CREATE DATABASE mijnDBPDO";
     // gebruik exec() want er wordt niets teruggegeven
-    $conn->exec($sql);
+    $conn->exec($sql_db);
     echo "Aanmaken database gelukt<br>";
     }
 catch(PDOException $e)
     {
-    echo $sql . "<br>" . $e->getMessage();
+    echo $sql_db . "<br>" . $e->getMessage();
     }
 
+// tabel maken
+    // gebruik exec() want er wordt niets teruggegeven
+    $conn->exec($sql_tabel);
+    echo "Aanmaken tabel gelukt";
+    }
+catch(PDOException $e)
+    {
+    echo $sql_tabel . "<br>" . $e->getMessage();
+    }
 
 ?> 
